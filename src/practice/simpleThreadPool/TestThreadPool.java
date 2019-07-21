accd65b078a3932af63dfaf007599a2347ae3328
zakree <957681484@qq.com>
2019-07-21 23:25:39 +0800

    简易线程池

--- src/practice/simpleThreadPool/TestThreadPool.java
+++ src/practice/simpleThreadPool/TestThreadPool.java
@@ -1,4 +1,4 @@
-package practice.threadPool;
+package practice.simpleThreadPool;
 
 
 public class TestThreadPool {
--- src/practice/simpleThreadPool/ThreadPool.java
+++ src/practice/simpleThreadPool/ThreadPool.java
@@ -1,4 +1,4 @@
-package practice.threadPool;
+package practice.simpleThreadPool;
 
 import java.util.HashSet;
 import java.util.Set;
