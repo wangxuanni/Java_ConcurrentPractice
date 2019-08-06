260d8c8cb3b1a2c3477dfc69c76ff6683b160085
zakree <957681484@qq.com>
2019-08-06 20:01:21 +0800

    Future模式

---
+++ src/practice/juc/future/myFuture/Data.java
@@ -0,0 +1,12 @@
+package practice.juc.future.myFuture;
+
+/**
+ * @parameter:
+ * @return:
+ * @description:
+ * @author: wangxuanni
+ * @create: 2019-08-04 17:13
+ **/
+public interface Data {
+    String getData();
+}
