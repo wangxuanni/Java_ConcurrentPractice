6e1bdfd23c3c48765b5a2aec31d4711fe8eb989d
zakree <957681484@qq.com>
2019-08-17 22:39:06 +0800

    工厂模式

---
+++ src/patterns/simple/DeilMouse.java
@@ -0,0 +1,13 @@
+package patterns.simple;
+
+/**
+ * @description:
+ * @author: wangxuanni
+ * @create: 2019-08-17 21:53
+ **/
+
+public class DeilMouse extends Mouse{
+    void sayHi(){
+        System.out.println("DeilMouse say hi");
+    }
+}
