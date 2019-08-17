af5ca8eba734e2b0ae88885e04e096095dee10cd
zakree <957681484@qq.com>
2019-08-17 22:39:58 +0800

    工厂模式

---
+++ src/patterns/simple/Mouse.java
@@ -0,0 +1,14 @@
+package patterns.simple;
+
+/**
+ * @parameter:
+ * @return:
+ * @description:
+ * @author: wangxuanni
+ * @create: 2019-08-17 21:51
+ **/
+public abstract class Mouse {
+    void sayHi(){
+
+    }
+}
