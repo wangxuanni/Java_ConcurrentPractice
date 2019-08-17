0305e002d1c33b41421e8e7fe7b2b10837d211f6
zakree <957681484@qq.com>
2019-08-17 22:39:45 +0800

    工厂模式

---
+++ src/patterns/simple/HpMouse.java
@@ -0,0 +1,14 @@
+package patterns.simple;
+
+/**
+ * @description:
+ * @author: wangxuanni
+ * @create: 2019-08-17 21:52
+ **/
+
+public class HpMouse extends Mouse{
+    @Override
+    void sayHi(){
+        System.out.println("HpMouse say hi");
+    }
+}
