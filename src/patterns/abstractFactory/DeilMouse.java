b706cdb4d40f7e96a85e121a7ff020d4d78e10ca
zakree <957681484@qq.com>
2019-08-19 11:19:06 +0800

    工厂模式

---
+++ src/patterns/abstractFactory/DeilMouse.java
@@ -0,0 +1,15 @@
+package patterns.abstractFactory;
+
+import patterns.simpleFactory.Mouse;
+
+/**
+ * @description:
+ * @author: wangxuanni
+ * @create: 2019-08-17 21:53
+ **/
+
+public class DeilMouse extends Mouse{
+    void sayHi(){
+        System.out.println("DeilMouse say hi");
+    }
+}
