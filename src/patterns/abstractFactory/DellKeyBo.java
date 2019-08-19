359ddfb8f2b71bf0dddc42b099e81a847d6ac40e
zakree <957681484@qq.com>
2019-08-19 11:19:27 +0800

    工厂模式

---
+++ src/patterns/abstractFactory/DellKeyBo.java
@@ -0,0 +1,14 @@
+package patterns.abstractFactory;
+
+/**
+ * @description:
+ * @author: wangxuanni
+ * @create: 2019-08-19 11:12
+ **/
+
+public class DellKeyBo extends KeyBo {
+    @Override
+    public void keyBoSayHi() {
+        System.out.println("DellKeyBo say hi");
+    }
+}
