d46f86dce3a2960bcf66dae4dc2b755dfe147e9e
zakree <957681484@qq.com>
2019-08-19 11:20:03 +0800

    工厂模式

---
+++ src/patterns/abstractFactory/HpKeyBo.java
@@ -0,0 +1,15 @@
+package patterns.abstractFactory;
+
+/**
+ * @description:
+ * @author: wangxuanni
+ * @create: 2019-08-19 11:13
+ **/
+
+public class HpKeyBo extends KeyBo {
+    @Override
+    public void keyBoSayHi() {
+        System.out.println("HpKeyBo say hi");
+
+    }
+}
