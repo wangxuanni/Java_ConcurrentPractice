7cf31963f46afc60d3e494cdf190b1dd80b2fa11
zakree <957681484@qq.com>
2019-08-19 11:20:22 +0800

    工厂模式

---
+++ src/patterns/abstractFactory/KeyBo.java
@@ -0,0 +1,12 @@
+package patterns.abstractFactory;
+
+/**
+ * @description:
+ * @author: wangxuanni
+ * @create: 2019-08-19 11:11
+ **/
+
+public  abstract class KeyBo {
+    public abstract void keyBoSayHi();
+
+}
